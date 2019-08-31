a6257ec4caa7c83773d968c6e6c51b58ad98e5ef
Daniel Kiesshau <danielkiesshau@outlook.com>
2019-08-31 15:04:18 -0300

    update user info

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -22,6 +22,36 @@ class UserController {
       provider,
     });
   }
+
+  async update(req, res) {
+    const { email, oldPassword } = req.body;
+    const user = await User.findByPk(req.userId);
+
+    if (user.email !== email) {
+      const userExists = await User.findOne({
+        where: {
+          email: req.body.email,
+        },
+      });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'User already exists' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(400).json({ error: "Password doesn't match" });
+    }
+
+    const { id, name, provider } = await user.update(req.body);
+
+    return res.json({
+      email,
+      id,
+      name,
+      provider,
+    });
+  }
 }
 
 export default new UserController();
