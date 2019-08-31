3eb0bf5dcd907decc45053623643ffd656866f00
Daniel Kiesshau <danielkiesshau@outlook.com>
2019-08-31 13:57:48 -0300

    user password bcrypted

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,26 @@
+import User from '../models/User';
+
+class UserController {
+  async store(req, res) {
+    const userExists = await User.findOne({
+      where: {
+        email: req.body.email,
+      },
+    });
+
+    if (userExists) {
+      return res.status(400).json({ error: 'User already exists' });
+    }
+
+    const { email, id, name, provider } = await User.create(req.body);
+
+    return res.json({
+      email,
+      id,
+      name,
+      provider,
+    });
+  }
+}
+
+export default new UserController();
