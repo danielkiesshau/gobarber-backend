ca5ba4f67f812feefb2bfc9a64039e7660e4c44d
Daniel Kiesshau <danielkiesshau@outlook.com>
2019-09-01 23:40:28 -0300

    nodemailer config

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'a02e9642323f28',
+    pass: 'b46e9279137f26',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
